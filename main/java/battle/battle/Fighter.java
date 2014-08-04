b748b11b1fbc6f023b5d0a5ec46dde07919318b1
Cade <Cade@localhost>
2014-08-04 01:50:07 -0500

    Basic game loops in place. Default fighters will swap blows until both die. main game loop uses the clock's ticks to pace the game.

--- main/java/battle/battle/Fighter.java
+++ main/java/battle/battle/Fighter.java
@@ -4,7 +4,29 @@ package battle.battle;
  * Created by Cade on 7/31/2014.
  */
 public class Fighter {
-    public Fighter(){
+    private String name;
+    private int str;
+    public int health;
 
+    public Fighter(){
+        this("default",1,50);
+    }
+
+    public Fighter(String n, int s, int h){
+        name = n;
+        str = s;
+        health = h;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getStr(){
+        return str;
+    }
+
+    public int getHealth(){
+        return health;
     }
 }
