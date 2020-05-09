7049a9060f181ebf7f46cb6eb2490d012cb98fac
Cade Rea <cade@caderea.com>
2020-05-09 00:58:16 -0500

    Remove unnecessary fields

--- app/src/main/java/com/caderea/battleapp/queue/DrawBlock.java
+++ app/src/main/java/com/caderea/battleapp/queue/DrawBlock.java
@@ -6,22 +6,16 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 import lombok.Getter;
-import lombok.Setter;
 
 class DrawBlock extends View {
     private final Paint backgroundPaint;
     private final Paint textPaint;
     @Getter private final QueueAction queueAction;
-    @Setter private float topLine;
-//    @Setter private float height;
 
     private int left;
     private int top;
     private int right;
     private int bottom;
-
-    private boolean measured = false;
-    private boolean layedout = false;
 
     public DrawBlock(Context context, QueueAction queueAction, int backgroundColor) {
         super(context);
@@ -37,22 +31,14 @@ class DrawBlock extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-//        float top = topLine;
-//        float left = 0;
-//        int right = getRight();
-//        float bottom = top + getHeight();
+
         canvas.drawRect(left, top, right, bottom, backgroundPaint);
         canvas.drawText(queueAction.getBattleAction().getName(), left + 5, bottom, textPaint);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(heightMeasureSpec);
-
-        setMeasuredDimension(width, height);
-        measured = true;
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
@@ -61,7 +47,6 @@ class DrawBlock extends View {
         this.top = top;
         this.right = right;
         this.bottom = bottom;
-        layedout = true;
     }
 
 }
