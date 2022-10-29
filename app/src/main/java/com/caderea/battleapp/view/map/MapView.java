cba158ce8ed4b131debd2280d845b951a53a2a9c
Cade Rea <cade@caderea.com>
2022-10-29 02:01:05 -0500

    Add zones to map

--- app/src/main/java/com/caderea/battleapp/view/map/MapView.java
+++ app/src/main/java/com/caderea/battleapp/view/map/MapView.java
@@ -2,32 +2,34 @@ package com.caderea.battleapp.view.map;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PointF;
+import android.graphics.*;
 import android.view.MotionEvent;
 import android.view.View;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 public class MapView extends View {
 
-    private float x, y, radius;
+    private float x, y, radius=100;
     private Paint paint;
 
     private PointF lastTouch;
+    private int height;
+    private int width;
+    private BattleRect[] zones;
+    private String message;
 
     public MapView(Context context) {
         super(context);
-        x=200;
-        y=200;
-        radius=100;
         paint = new Paint();
+        paint.setTextSize(100);
         lastTouch = new PointF();
+        message = "Hello!";
+        paint.setColor(Color.parseColor("#ff0000ff"));
 
         setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                paint.setColor(Color.parseColor("#ff0000ff"));
                 v.invalidate();
             }
         });
@@ -39,6 +41,13 @@ public class MapView extends View {
 
                 if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
                     lastTouch.set(event.getX(), event.getY());
+
+                    for (BattleRect zone : zones) {
+                        if (zone.getRect().contains((int) event.getX(), (int) event.getY())) {
+                            message = "" + zone.getPaint().getColor();
+                        }
+                    }
+
                 }
 
                 return false;
@@ -46,10 +55,44 @@ public class MapView extends View {
         });
     }
 
+    private void init() {
+        height = getHeight();
+        width = getWidth();
+
+        zones = new BattleRect[]{
+                getRect(0, 0, width / 2, height / 2, "#88ff0000"),
+                getRect(width / 2, 0, width, height / 2, "#8800ff00")};
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawText("Hello!", x + 100, y + 100, paint);
+        if (height == 0 && width == 0) {
+            init();
+        }
+
+        canvas.drawText(message, height / 3, width / 3, paint);
         canvas.drawCircle(lastTouch.x, lastTouch.y, radius, paint);
+
+        for (BattleRect zone : zones) {
+            canvas.drawRect(zone.getRect(), zone.getPaint());
+        }
+    }
+
+    private BattleRect getRect(int left, int top, int right, int bottom, String colorString) {
+
+        Rect rect = new Rect(left, top, right, bottom);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor(colorString));
+
+        return new BattleRect(rect, paint);
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class BattleRect {
+        Rect rect;
+        Paint paint;
     }
 
     @Override
